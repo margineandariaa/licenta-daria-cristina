e7d0064bda6e3a3ff63d45e950f383b6e2a8e246
Mărginean Daria-Maria <dariamarginean@Margineans-MacBook-Air.local>
2026-02-23 23:11:35 +0200

    Remove bin and obj folders

--- licenta-backend/Licenta.Api/obj/Debug/net8.0/Licenta.Api.AssemblyInfo.cs
+++ licenta-backend/Licenta.Api/obj/Debug/net8.0/Licenta.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Licenta.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aefbabe738a163f2ae93528cb7c8d76889447a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Licenta.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Licenta.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
